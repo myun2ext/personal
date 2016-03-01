8d1436d20cabff5971073b789d7f58c5679dd11d
Tomoaki Yahagi <yahagi@fact-real.com>
2016-03-01 18:51:05 +0900

    Add migration

---
+++ init.rb
@@ -0,0 +1,12 @@
+require 'activerecord'
+class CreateTraitMigration < ActiveRecord::Migration
+  def change
+    create_table(:traits) do |t|
+      t.string    :what,          null: false
+      t.string    :of,            null: false
+      t.string    :is,            null: false
+      t.datetime  :remembered_at, null: false
+      t.index     %i[what of], unique: true
+    end
+  end
+end
